192ffb50499ca5c7c10d7c036f8792dce093a4d2
ThisIsBucca <buccaphilox0104@gmail.com>
2026-04-23 09:35:58 -0700

    wanene

--- settings.js
+++ settings.js
@@ -1,9 +1,8 @@
 module.exports = {
-  botOwner: "Azar",
-  ownerNumber: "917358646943",
+  botOwner: "Bucca",
+  ownerNumber: "255778286840",
   prefix: ".",
   version: "5.2.1",
   commandMode: "public",
   maxStoreMessages: 200,
-  storeWriteInterval: 10000
-};
+  storeWriteInter
